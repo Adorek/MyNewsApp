50a88943c9d4deaafc7ce4abe600d1168f0bfebe
Adorek <adam.rafinski@gmail.com>
2018-06-06 22:30:57 +0200

    Add application settings of "order by" and "News count"

--- app/src/main/java/com/example/android/mynewsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/mynewsapp/NewsActivity.java
@@ -4,11 +4,16 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -23,33 +28,53 @@ public class NewsActivity extends AppCompatActivity
 
     /** URL for articles data from the Guardian API */
     private static final String NEWS_URL =
-            "http://content.guardianapis.com/search?&order-by=newest&page-size=25&show-fields=headline,trailText,firstPublicationDate,shortUrl,thumbnail,byline&api-key=test";
+            "http://content.guardianapis.com/search";
 
-
-    /** TextView that is displayed when the list is empty */
-    private TextView mEmptyStateTextView;
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int ARTICLE_LOADER_ID = 1;
 
     /** Adapter for the list of articles */
     private ArticleAdapter mArticleAdapter;
 
-    /**Show progress ring when the data is loading */
-    private ProgressBar mProgressBar;
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
+
+
+    @Override
+    // This method initialize the contents of the Activity's options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    // This method is called whenever an item in the options menu is selected.
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
-        mProgressBar = (ProgressBar) findViewById(R.id.loading_progress_bar);
-
         // Find a reference to the {@link ListView} in the layout
         ListView articlesListView = (ListView) findViewById(R.id.news_list);
 
-        // Create a new adapter that takes an empty list of articles as input
-        mArticleAdapter = new ArticleAdapter(this, new ArrayList<Article>());
-
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_text_view);
         articlesListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter that takes an empty list of articles as input
+        mArticleAdapter = new ArticleAdapter(this, new ArrayList<Article>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
@@ -77,32 +102,77 @@ public class NewsActivity extends AppCompatActivity
         // Get details on the currently active default data network
         NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
 
-        LoaderManager loaderManager = getLoaderManager();
+
 
         // If there is a network connection, fetch data
         if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
-            loaderManager.initLoader(1, null, this);
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
         } else {
-            mProgressBar.setVisibility(View.GONE);
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_progress_bar);
+            loadingIndicator.setVisibility(View.GONE);
+
             // Update empty state with no connection error message
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
     }
 
     @Override
+    // onCreateLoader instantiates and returns a new Loader for the given ID
     public Loader<List<Article>> onCreateLoader(int i, Bundle bundle) {
-        // Create a new loader for the given URL
-        return new ArticleLoader(this, NEWS_URL);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences. The second parameter is the
+        // default value for this preference.
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+
+        String minArticlesNumber = sharedPrefs.getString(
+                getString(R.string.settings_min_articles_key),
+                getString(R.string.settings_min_articles_default));
+
+        String fields = getString(R.string.URL_fields_keywords);
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(NEWS_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the `order-by=newest`
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("page-size", minArticlesNumber);
+        uriBuilder.appendQueryParameter("show-fields", fields);
+        uriBuilder.appendQueryParameter("api-key", "test");
+
+        // Return the completed uri `http://content.guardianapis.com/search?order-by=newest&page-size=25&show-fields=headline,trailText,firstPublicationDate,shortUrl,thumbnail,byline&api-key=test'
+        return new ArticleLoader(this, uriBuilder.toString());
     }
 
     @Override
     public void onLoadFinished(Loader<List<Article>> loader, List<Article> articles) {
+
         // Hide loading indicator
-        mProgressBar.setVisibility(View.GONE);
+        View loadingIndicator = findViewById(R.id.loading_progress_bar);
+        loadingIndicator.setVisibility(View.GONE);
+
         // Set empty state text to display "No articles found."
         mEmptyStateTextView.setText(R.string.no_articles_found);
+
         // Clear the adapter of previous articles
         mArticleAdapter.clear();
+
+        // If there is a valid list of {@link Article}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
         if (articles != null && !articles.isEmpty()) {
             mArticleAdapter.addAll(articles);
         }
